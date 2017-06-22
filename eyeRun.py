58ebf3d1f74060c80dce577453b386667edf20ec
Kevin Fang <kfang1233@gmail.com>
2017-06-22 10:56:44 -0400

    Moved image to color to CSV, created updated eyeRun.py and jupyter notebook

--- eyeRun.py
+++ eyeRun.py
@@ -1,8 +1,56 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
 from sklearn.svm import SVC
 import numpy as np
 import pandas as pd
+import pickle
 
-columns = ['left', 'right']
+
+# In[63]:
+
+
+# read names that have provided survey eye color data
+columns = ['name', 'left', 'right', 'left_desc', 'right_desc']
 data = pd.read_csv("PGP-eyecolor.csv", names=columns)
-data.columns = columns
-print data.head()
+dataNames = data['name'].values.tolist()
+
+
+# In[72]:
+
+
+# load numpy arrays from Arvados collection
+names = np.load("names.npy")
+pgp = np.load("hiq-pgp.npy")
+for i in range(len(names)):
+    names[i] = names[i][:8]
+
+
+# In[101]:
+
+
+nameIndices = []
+for i in range(len(names)):
+    if names[i] in dataNames:
+        nameIndices.append(i)
+        
+knownData = pgp[nameIndexes]
+unknownData = np.delete(pgp, nameIndices)
+unknownData.reshape(len(names), knownData.shape[1] - 1)
+
+
+# In[ ]:
+
+
+print knownData.shape
+print unknownData.shape
+
+
+# In[ ]:
+
+
+
+
