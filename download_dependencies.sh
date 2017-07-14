02154f131659c7e062939ec780de4e81d2194714
Kevin Fang <kfang@commschool.org>
2017-07-14 14:31:38 -0400

    Fixed dependency download

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,5 +1,5 @@
 echo "Downloading..."
 arv-get bfba01c5b9b4053f596a2dc36d072cd6+46480/hiq-pgp .
 arv-get bfba01c5b9b4053f596a2dc36d072cd6+46480/names .
-cd tiling && arv-get b8835cbca4f8dfd3396f39f5ca10bb84+780/assembly.00.hg19.fw.fwi . && arv-get b8835cbca4f8dfd3396f39f5ca10bb84+780/assembly.00.hg19.fw.gz . && arv-get b8835cbca4f8dfd3396f39f5ca10bb84+780/assembly.00.hg19.fw.gz.gzi . && arv-get bfba01c5b9b4053f596a2dc36d072cd6+46480/hiq-pgp-info .
+cd tiling_files && arv-get b8835cbca4f8dfd3396f39f5ca10bb84+780/assembly.00.hg19.fw.fwi . && arv-get b8835cbca4f8dfd3396f39f5ca10bb84+780/assembly.00.hg19.fw.gz . && arv-get b8835cbca4f8dfd3396f39f5ca10bb84+780/assembly.00.hg19.fw.gz.gzi . && arv-get bfba01c5b9b4053f596a2dc36d072cd6+46480/hiq-pgp-info .
 echo "Done downloading dependencies"
